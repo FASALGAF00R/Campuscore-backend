4eb5e7feae8d92aa1a81eba43d043f59fa6d564b
FASALGAFOOR <fasalgafoor2080@gmail.com>
2026-02-27 17:26:33 +0530

    fixed token issuess

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -3,14 +3,14 @@ import jwt from 'jsonwebtoken';
 // Generate Access Token (short-lived)
 export const generateAccessToken = (userId, role) => {
   return jwt.sign({ id: userId, role }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_ACCESS_EXPIRE || '15m',
+    expiresIn: process.env.JWT_ACCESS_EXPIRE || '10d',
   });
 };
 
 // Generate Refresh Token (long-lived)
 export const generateRefreshToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_REFRESH_SECRET, {
-    expiresIn: process.env.JWT_REFRESH_EXPIRE || '7d',
+    expiresIn: process.env.JWT_REFRESH_EXPIRE || '20d',
   });
 };
 
